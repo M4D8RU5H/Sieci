2ec2724a671cd144cfa5d2dcef3c06a37ddb8726
madbrush <getkrzysiek200@gmail.com>
2023-03-27 23:31:29 +0200

    Done!

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from GUI.app import App
+
 from algorithms.rail_fence import RailFence
+from algorithms.example_a import ExampleA
+from algorithms.example_b import ExampleB
+from algorithms.example_c import ExampleC
 from algorithms.ceasar import Ceasar
 from algorithms.vigenere import Vigenere
 
 if __name__ == "__main__":
-    app = App([Vigenere()])
+    app = App([
+        RailFence(),
+        ExampleA(),
+        ExampleB(),
+        ExampleC(),
+        Ceasar(),
+        Vigenere()])
+
     app.mainloop()
